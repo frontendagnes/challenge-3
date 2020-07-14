5013053ee61face0a3808244c7cd6684f2dccc7d
Agnieszka Kaminska <agnieszka.kaminska@ksiegarnia.edu.pl>
2020-07-14 13:05:32 +0200

    css

---
+++ script.js
@@ -0,0 +1,14 @@
+const navHead = document.querySelector(".nav-head");
+const menuCnt = document.querySelector(".menu-cnt")
+
+function sticky(){
+    if (window.scrollY > 72){
+        navHead.classList.add("sticky")
+        menuCnt.classList.add("menu-sticky")
+    }else{
+        navHead.classList.remove("sticky")
+        menuCnt.classList.remove("menu-sticky")
+    }
+}
+
+window.addEventListener('scroll', sticky)
